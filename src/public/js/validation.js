8edbc4e8d78615d899966ffbc00152ff102fa4f8
suzuki miyu <bu.m6s.pq@gamil.com>
2023-11-25 11:40:35 +0900

    テスト後自習

--- src/public/js/validation.js
+++ src/public/js/validation.js
@@ -1,20 +1,29 @@
-function setupFocusEvent(inputName, jsErrorId) {
-    var inputField = document.querySelector('input[name="' + inputName + '"]');
-    var jsErrorElement = document.getElementById(jsErrorId);
+document.addEventListener('DOMContentLoaded', function () {
+    // DOMが完全に読み込まれた後に実行される処理
 
-    // フォームに入力した時のイベント
-    inputField.addEventListener('blur', function () {
-        // バリデーション
-        if (this.value.trim() === '') {
-            displayErrorMessage(jsErrorElement, "この項目は必須です。");
-        } else {
-            displayErrorMessage(jsErrorElement, ""); // エラーメッセージをクリア
-        }
-    });
-}
+    function setupFocusEvent(inputName, jsErrorId) {
+        var inputField = document.querySelector('input[name="' + inputName + '"]');
+        var jsErrorElement = document.getElementById(jsErrorId);
 
-setupFocusEvent('family-name', 'js-family-name-error');
-setupFocusEvent('given-name', 'js-given-name-error');
-setupFocusEvent('email', 'js-email-error');
-setupFocusEvent('address', 'js-address-error');
-setupFocusEvent('postcode', 'js-postcode-error');
+        // フォームに入力した時のイベント
+        inputField.addEventListener('blur', function () {
+            // バリデーション
+            if (this.value.trim() === '') {
+                displayErrorMessage(jsErrorElement, "この項目は必須です。");
+            } else {
+                displayErrorMessage(jsErrorElement, ""); // エラーメッセージをクリア
+            }
+        });
+    }
+
+    function displayErrorMessage(element, message) {
+        var errorContainer = element.parentElement.querySelector('.form__error');
+        errorContainer.innerHTML = message;
+    }
+
+    setupFocusEvent('family-name', 'js-family-name-error');
+    setupFocusEvent('given-name', 'js-given-name-error');
+    setupFocusEvent('email', 'js-email-error');
+    setupFocusEvent('address', 'js-address-error');
+    setupFocusEvent('postcode', 'js-postcode-error');
+});
